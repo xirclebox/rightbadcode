b780e01811306074a803844975c3938287f68f71
Homer Gaines <xirclebox@gmail.com>
2026-02-22 01:02:24 -0500

    added react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), react()],
 });
