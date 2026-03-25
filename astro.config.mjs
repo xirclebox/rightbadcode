fe6a6fcc95d0210cc6430fb2da627751d76682a4
Homer Gaines <xirclebox@gmail.com>
2026-03-25 12:01:38 -0400

    post test 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://rightbadcode.com',
     integrations: [mdx(), sitemap(), react(), partytown()],
 });
