29533464a107b5911f02ca954fecc61ce2ab4773
Homer Gaines <xirclebox@gmail.com>
2026-03-25 22:24:41 -0400

    updated description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "RightBadCode";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Right Bad Code is a web accessibility blog with practical guidance on WCAG compliance, inclusive design, semantic HTML, and building accessible digital products.";
