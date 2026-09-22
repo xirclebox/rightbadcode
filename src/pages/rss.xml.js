1972a4e233fa499b5ee55b8f4d5b47b481cfe5be
Homer Gaines <xirclebox@gmail.com>
2026-09-22 16:51:28 -0400

    rss update

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,19 +1,17 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	const publishedPosts = posts.filter(
-    (post) => post.data.draft !== true
-  );
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: publishedPosts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const posts = await getCollection("blog");
+  const publishedPosts = posts.filter((post) => post.data.draft !== true);
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: publishedPosts.map((post) => ({
+      ...post.data,
+      link: `/${post.id}/`,
+    })),
+  });
 }
