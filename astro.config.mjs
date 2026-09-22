bfa29e11479c88e939ce673bee5fea03bf8d2fa4
Homer Gaines <xirclebox@gmail.com>
2026-09-22 17:00:37 -0400

    analytics update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import partytown from '@astrojs/partytown';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://rightbadcode.com',
-    integrations: [mdx(), sitemap(), react(), partytown()],
+  site: "https://rightbadcode.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
